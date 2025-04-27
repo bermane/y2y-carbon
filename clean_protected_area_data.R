6bb9f39e782b55f2dd7f07941338f4999c63e688
Ethan Berman <bermane@gmail.com>
2025-04-27 12:24:22 -0600

    removed wpda we didn't use

--- clean_protected_area_data.R
+++ clean_protected_area_data.R
@@ -5,28 +5,15 @@
 library(sf)
 library(wdpar)
 
-############################
-### WDPA_WDOECM JAN 2025 ###
-############################
+###########################
+## WDPA_WDOECM JAN 2025 ###
+###########################
 
-# # read WDPA_WDOECM shapefile
-# wdpa_wdoecm <- st_read('/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/protected_areas_WDPA_WDOECM.shp')
-# 
-# # clean WDPA data
-# wdpa_wdoecm_clean <- wdpa_clean(wdpa_wdoecm, geometry_precision = 10000)
-# 
-# # export to disk
-# st_write(wdpa_wdoecm_clean, '/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/protected_areas_WDPA_WDOECM_clean.shp')
-
-#######################
-### WDPA MARCH 2025 ###
-#######################
-
-# read WDPA shapefile
-wdpa <- st_read('/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/wdpa_y2y_raw.shp')
+# read WDPA_WDOECM shapefile
+wdpa_wdoecm <- st_read('/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/protected_areas_WDPA_WDOECM.shp')
 
 # clean WDPA data
-wdpa_clean <- wdpa_clean(wdpa, geometry_precision = 10000)
+wdpa_wdoecm_clean <- wdpa_clean(wdpa_wdoecm, geometry_precision = 10000)
 
 # export to disk
-st_write(wdpa_clean, '/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/wdpa_y2y_clean.shp')
+st_write(wdpa_wdoecm_clean, '/Users/ethanberman/Team Braintree Dropbox/Ethan Berman/Python Projects/y2y-carbon/land_cover/protected_areas_WDPA_WDOECM_clean.shp')
